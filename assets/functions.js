c36dbbac46684630e452fea9827051a896ccfbf6
jyyuiz <john.hyunchan@gmail.com>
2017-07-26 20:17:48 -0600

    Added highlighting for selected items in list

--- assets/functions.js
+++ assets/functions.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
-	//$(
-	
-	
-	
+
+$(document).on('click', ".lineName", function(e) {
+	$(this).nextAll().eq(1).slideToggle("slow");//toggles modifiers
 });
 
-$(document).on('click', ".lineName", function() {
-	$(this).nextAll().eq(1).slideToggle("slow");
+var $cols = $(document).on('click', ".lineName", function(e) {
+	var highlight = 'selected';
+	$(this).toggleClass(highlight);//add or remove highlight
 });
+
